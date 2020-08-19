cb8cec967a85c274e8fa9e9a58f1742ce192ee89
Mingfun Wong <mingfun.wong.chn@gmail.com>
2020-08-20 08:44:20 +0900

    Update save.php

--- save.php
+++ save.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 error_reporting(0);
 set_time_limit(60);
 
@@ -10,14 +10,20 @@ $basename = from($pathinfo, 'basename', 'index.html');
 $fullpath = ".{$dirname}/{$basename}";
 
 if (file_exists($fullpath)) {
-	exit(file_get_contents($fullpath));
+  header('HTTP/1.1 200 OK');
+  header('Content-type: ' . mime_content_type($fullpath));
+  exit(file_get_contents($fullpath));
 }
 
 $target_url = "{$site_url}{$redirect_url}";
 $contents = curl($target_url);
 $contents = str_replace($site_url, "", $contents);
 mk_dir(".{$dirname}");
-file_put_contents("{$fullpath}", $contents);
+if ($contents) {
+  header('HTTP/1.1 200 OK');
+  file_put_contents("{$fullpath}", $contents);
+  header('Content-type: ' . mime_content_type($fullpath));
+}
 exit($contents);
 
 /**
@@ -28,52 +34,53 @@ exit($contents);
  * @param int $mode
  * @return bool
  */
-function mk_dir($dir, $mode = 0755) 
-{ 
-    if (is_dir($dir) || @mkdir($dir, $mode)) return true; 
-    if (!mk_dir(dirname($dir), $mode)) return false; 
-    return @mkdir($dir, $mode); 
+function mk_dir($dir, $mode = 0755)
+{
+  if (is_dir($dir) || @mkdir($dir, $mode)) return true;
+  if (!mk_dir(dirname($dir), $mode)) return false;
+  return @mkdir($dir, $mode);
 }
 
 /**
-* 获得数组指定键的值
-*
-* @param array,object $array
-* @param string $key
-* @param mixed $default
-* @return mixed
-*/
+ * 获得数组指定键的值
+ *
+ * @param array,object $array
+ * @param string $key
+ * @param mixed $default
+ * @return mixed
+ */
 function from($array, $key, $default = FALSE)
 {
-	$return = $default;
-	if (is_object($array)) $return = (isset($array->$key) === TRUE && empty($array->$key) === FALSE) ? $array->$key : $default;
-	if (is_array($array)) $return = (isset($array[$key]) === TRUE && empty($array[$key]) === FALSE) ? $array[$key] : $default;
+  $return = $default;
+  if (is_object($array)) $return = (isset($array->$key) === TRUE && empty($array->$key) === FALSE) ? $array->$key : $default;
+  if (is_array($array)) $return = (isset($array[$key]) === TRUE && empty($array[$key]) === FALSE) ? $array[$key] : $default;
 
-	return $return;
-} 
+  return $return;
+}
 
 // curl 数据
-function curl($url, $method = "GET", $data = ''){
-    $ch = curl_init(); 
-    curl_setopt($ch, CURLOPT_URL, $url); 
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE); 
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
-    curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (compatible; MSIE 5.01; Windows NT 5.0)');
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-    curl_setopt($ch, CURLOPT_AUTOREFERER, 1); 
-    curl_setopt($ch, CURLOPT_TIMEOUT, 60); 
+function curl($url, $method = "GET", $data = '')
+{
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+  curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (compatible; MSIE 5.01; Windows NT 5.0)');
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+  curl_setopt($ch, CURLOPT_AUTOREFERER, 1);
+  curl_setopt($ch, CURLOPT_TIMEOUT, 60);
 
-    if ($data) {
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-    }
+  if ($data) {
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+  }
 
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
-    $info = curl_exec($ch);
-    if (curl_errno($ch)) {
-        echo 'Errno'.curl_error($ch);
-    }
-    curl_close($ch);
-    return $info;
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  $info = curl_exec($ch);
+  if (curl_errno($ch)) {
+    echo 'Errno' . curl_error($ch);
+  }
+  curl_close($ch);
+  return $info;
 }
